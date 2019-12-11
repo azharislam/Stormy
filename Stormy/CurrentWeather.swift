9f72f4131e52c12bd2a40032bf24fbb776d249f0
Azhar Islam <azhar.islam@ynap.com>
2019-12-11 17:04:23 +0000

    implemented codable

--- Stormy/CurrentWeather.swift
+++ Stormy/CurrentWeather.swift
@@ -9,12 +9,13 @@
 import Foundation
 import UIKit
 
-struct CurrentWeather {
+struct CurrentWeather: Codable {
     let temperature: Double
     let humidity: Double
     let precipProbability: Double
     let summary: String
     let icon: String
+    
 }
 
 extension CurrentWeather {
@@ -32,32 +33,5 @@ extension CurrentWeather {
         case "partly-cloudy-night": return #imageLiteral(resourceName: "partly-cloudy-night")
         default: return #imageLiteral(resourceName: "default")
         }
-    }
-}
-
-extension CurrentWeather {
-    struct Key {
-        static let temperature = "temperature"
-        static let humidity = "humidity"
-        static let precipitationProbability = "precipProbability"
-        static let summary = "summary"
-        static let icon = "icon"
-    }
-    
-    
-    init?(json: [String: AnyObject]) {
-        guard let tempValue = json[Key.temperature] as? Double,
-        let humidityValue = json[Key.humidity] as? Double,
-        let precipitationProbabilityValue = json[Key.precipitationProbability] as? Double,
-        let summaryString = json[Key.summary] as? String,
-            let iconString = json[Key.icon] as? String else {
-                return nil
-        }
-        
-        self.temperature = tempValue
-        self.humidity = humidityValue
-        self.precipProbability = precipitationProbabilityValue
-        self.summary = summaryString
-        self.icon = iconString
     }
 }
